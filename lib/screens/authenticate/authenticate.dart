6f954d4b9d56e3ceee2411a605f738289e331976
Bank <phanukorn.khankrua@gmail.com>
2021-05-14 10:32:17 +0700

    initial commit

---
+++ lib/screens/authenticate/authenticate.dart
@@ -0,0 +1,16 @@
+import 'package:brew_crew/screens/authenticate/sign_in.dart';
+import 'package:flutter/material.dart';
+
+class Autenticate extends StatefulWidget {
+  @override
+  _AutenticateState createState() => _AutenticateState();
+}
+
+class _AutenticateState extends State<Autenticate> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: SignIn(),
+    );
+  }
+}
