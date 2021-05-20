bb302776a5dd89169c0962e4333921ffbf6e6b78
Bank <phanukorn.khankrua@gmail.com>
2021-05-20 13:10:57 +0700

    Done!

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -52,7 +52,15 @@ class Home extends StatelessWidget {
               label: Text('settings'))
           ],
         ),
-        body: BrewList(),
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/coffee_bg.png'),
+              fit: BoxFit.cover,
+              ),
+            ),
+          child: BrewList()
+          ),
       ),
     );
     
